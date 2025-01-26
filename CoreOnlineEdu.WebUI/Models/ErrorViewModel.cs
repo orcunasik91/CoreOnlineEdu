180bf873074fdac2c4300df4351a659739f90fe7
Orçun Aşık <asikorcun@gmail.com>
2025-01-26 14:55:07 +0300

    Created project layers in solution

---
+++ CoreOnlineEdu.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreOnlineEdu.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
