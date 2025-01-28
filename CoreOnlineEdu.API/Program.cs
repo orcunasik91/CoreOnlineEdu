0e01c5c3a629da367c0b674e75df532f49f6d2cd
Orçun Aşık <asikorcun@gmail.com>
2025-01-28 17:59:08 +0300

    Project refeProject references were given and ef core packages were added and db was created with migrationrenceses

--- CoreOnlineEdu.API/Program.cs
+++ CoreOnlineEdu.API/Program.cs
@@ -1,15 +1,16 @@
+using CoreOnlineEdu.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<OnlineEduContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
