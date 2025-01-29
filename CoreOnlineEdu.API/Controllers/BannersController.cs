a3b4a809c57e4d61e03a375ba61eddcd187f11bc
Orçun Aşık <asikorcun@gmail.com>
2025-01-29 22:22:23 +0300

    Added controller, mapping and dtos for Banner

---
+++ CoreOnlineEdu.API/Controllers/BannersController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CoreOnlineEdu.Business.Abstracts;
+using CoreOnlineEdu.DTO.Dtos.BannerDtos;
+using CoreOnlineEdu.Entity.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreOnlineEdu.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BannersController(IBaseService<Banner> bannerService, IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        List<Banner> result = bannerService.GetList();
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        Banner result = bannerService.GetById(id);
+        return Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        bannerService.Delete(id);
+        return Ok("Banner Silindi!");
+    }
+
+    [HttpPost]
+    public IActionResult Create(CreateBannerDto createBannerDto)
+    {
+        Banner banner = mapper.Map<Banner>(createBannerDto);
+        bannerService.Create(banner);
+        return Ok("Yeni Banner Oluşturuldu");
+    }
+
+    [HttpPut]
+    public IActionResult Update(UpdateBannerDto updateBannerDto)
+    {
+        Banner banner = mapper.Map<Banner>(updateBannerDto);
+        bannerService.Update(banner);
+        return Ok("Banner Güncellendi");
+    }
+}
---
+++ CoreOnlineEdu.API/Mapping/BannerMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CoreOnlineEdu.DTO.Dtos.BannerDtos;
+using CoreOnlineEdu.Entity.Entities;
+
+namespace CoreOnlineEdu.API.Mapping;
+public class BannerMapping : Profile
+{
+    public BannerMapping()
+    {
+        CreateMap<CreateBannerDto, Banner>().ReverseMap();
+        CreateMap<UpdateBannerDto, Banner>().ReverseMap();
+    }
+}
