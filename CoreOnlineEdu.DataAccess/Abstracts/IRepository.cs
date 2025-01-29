2c120b9ab11021cfa9d2db63715a5532a7e37caa
Orçun Aşık <asikorcun@gmail.com>
2025-01-29 17:50:53 +0300

    Repository Pattern has been implemented

---
+++ CoreOnlineEdu.DataAccess/Abstracts/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace CoreOnlineEdu.DataAccess.Abstracts;
+public interface IRepository<T> where T :  class
+{
+    List<T> GetList();
+    List<T> GetFilteredList(Expression<Func<T, bool>> predicate);
+    T GetById(int id);
+    T GetByFilter(Expression<Func<T, bool>> predicate);
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(int id);
+    int Count();
+    int FilteredCount(Expression<Func<T, bool>> predicate);
+}
---
+++ CoreOnlineEdu.DataAccess/Repositories/Repository.cs
@@ -0,0 +1,58 @@
+﻿using CoreOnlineEdu.DataAccess.Abstracts;
+using CoreOnlineEdu.DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CoreOnlineEdu.DataAccess.Repositories;
+public class Repository<T>(OnlineEduContext context) : IRepository<T> where T : class
+{
+    public DbSet<T> Table { get=> context.Set<T>(); }
+    public int Count()
+    {
+        return Table.Count();
+    }
+
+    public void Create(T entity)
+    {
+        Table.Add(entity);
+        context.SaveChanges();
+    }
+
+    public void Delete(int id)
+    {
+        T entity = GetById(id);
+        Table.Remove(entity);
+        context.SaveChanges();
+    }
+
+    public int FilteredCount(Expression<Func<T, bool>> predicate)
+    {
+        return Table.Count(predicate);
+    }
+
+    public T GetByFilter(Expression<Func<T, bool>> predicate)
+    {
+        return Table.FirstOrDefault(predicate);
+    }
+
+    public T GetById(int id)
+    {
+        return Table.Find(id);
+    }
+
+    public List<T> GetFilteredList(Expression<Func<T, bool>> predicate)
+    {
+        return Table.Where(predicate).ToList();
+    }
+
+    public List<T> GetList()
+    {
+        return Table.ToList();
+    }
+
+    public void Update(T entity)
+    {
+        Table.Update(entity);
+        context.SaveChanges();
+    }
+}
