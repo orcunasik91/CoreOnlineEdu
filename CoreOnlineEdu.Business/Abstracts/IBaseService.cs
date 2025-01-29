79353e50c8c56ecb0ee60690738d42e80006c5b3
Orçun Aşık <asikorcun@gmail.com>
2025-01-29 18:11:15 +0300

    Implemented a generic BaseService configuration

---
+++ CoreOnlineEdu.Business/Abstracts/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace CoreOnlineEdu.Business.Abstracts;
+public interface IBaseService<TEntity> where TEntity : class
+{
+    List<TEntity> GetList();
+    List<TEntity> GetFilteredList(Expression<Func<TEntity, bool>> predicate);
+    TEntity GetById(int id);
+    TEntity GetByFilter(Expression<Func<TEntity, bool>> predicate);
+    void Create(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(int id);
+    int Count();
+    int FilteredCount(Expression<Func<TEntity, bool>> predicate);
+}
---
+++ CoreOnlineEdu.Business/Concretes/BaseService.cs
@@ -0,0 +1,52 @@
+﻿using CoreOnlineEdu.Business.Abstracts;
+using CoreOnlineEdu.DataAccess.Abstracts;
+using System.Linq.Expressions;
+
+namespace CoreOnlineEdu.Business.Concretes;
+public class BaseService<TEntity>(IRepository<TEntity> repository) : IBaseService<TEntity> where TEntity : class
+{
+    public int Count()
+    {
+       return repository.Count();
+    }
+
+    public void Create(TEntity entity)
+    {
+        repository.Create(entity);
+    }
+
+    public void Delete(int id)
+    {
+        repository.Delete(id);
+    }
+
+    public int FilteredCount(Expression<Func<TEntity, bool>> predicate)
+    {
+        return repository.FilteredCount(predicate);
+    }
+
+    public TEntity GetByFilter(Expression<Func<TEntity, bool>> predicate)
+    {
+        return repository.GetByFilter(predicate);
+    }
+
+    public TEntity GetById(int id)
+    {
+        return repository.GetById(id);
+    }
+
+    public List<TEntity> GetFilteredList(Expression<Func<TEntity, bool>> predicate)
+    {
+        return repository.GetFilteredList(predicate);
+    }
+
+    public List<TEntity> GetList()
+    {
+        return repository.GetList();
+    }
+
+    public void Update(TEntity entity)
+    {
+        repository.Update(entity);
+    }
+}
