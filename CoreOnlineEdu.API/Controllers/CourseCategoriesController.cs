fb68b63b4df60a2ffc17c56590b8df3f32f26752
Orçun Aşık <asikorcun@gmail.com>
2025-02-01 23:04:16 +0300

    Added controller, mapping and dtos for CourseCategory

---
+++ CoreOnlineEdu.API/Controllers/CourseCategoriesController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CoreOnlineEdu.Business.Abstracts;
+using CoreOnlineEdu.DTO.Dtos.CourseCategoryDtos;
+using CoreOnlineEdu.Entity.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreOnlineEdu.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CourseCategoriesController(IBaseService<CourseCategory> courseCategoryService, IMapper mapper) : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        List<CourseCategory> result = courseCategoryService.GetList();
+        return Ok(result);
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        CourseCategory result = courseCategoryService.GetById(id);
+        return Ok(result);
+    }
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        courseCategoryService.Delete(id);
+        return Ok("KursKategori Silindi!");
+    }
+    [HttpPost]
+    public IActionResult Create(CreateCourseCategoryDto createCourseCategoryDto)
+    {
+        CourseCategory courseCategory = mapper.Map<CourseCategory>(createCourseCategoryDto);
+        courseCategoryService.Create(courseCategory);
+        return Ok("Yeni KursKategori Oluşturuldu");
+    }
+    [HttpPut]
+    public IActionResult Update(UpdateCourseCategoryDto updateCourseCategoryDto)
+    {
+        CourseCategory courseCategory = mapper.Map<CourseCategory>(updateCourseCategoryDto);
+        courseCategoryService.Update(courseCategory);
+        return Ok("KursKategori Güncellendi");
+    }
+}
---
+++ CoreOnlineEdu.API/Mapping/CourseCategoryMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CoreOnlineEdu.DTO.Dtos.CourseCategoryDtos;
+using CoreOnlineEdu.Entity.Entities;
+
+namespace CoreOnlineEdu.API.Mapping;
+public class CourseCategoryMapping : Profile
+{
+    public CourseCategoryMapping()
+    {
+        CreateMap<CreateCourseCategoryDto, CourseCategory>().ReverseMap();
+        CreateMap<UpdateCourseCategoryDto, CourseCategory>().ReverseMap();
+    }
+}
