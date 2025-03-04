2f5587bf759043626aa7c99bdd9512fafedb815e
Orçun Aşık <asikorcun@gmail.com>
2025-03-04 22:19:44 +0300

    editied Index action

--- CoreOnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
+++ CoreOnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreOnlineEdu.WebUI.Dtos.AboutDtos;
+using CoreOnlineEdu.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreOnlineEdu.WebUI.Areas.Admin.Controllers;
 
 [Area("Admin")]
+[Route("[area]/[controller]/[action]/{id?}")]
 public class AboutController : Controller
 {
-    public IActionResult Index()
+    private readonly HttpClient client = HttpClientInstance.CreateClient();
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var result = await client.GetFromJsonAsync<List<ResultAboutDto>>("Abouts");
+        return View(result);
     }
 }
