449c9f7428234ebd405ba0ca37105a9ddcf81613
Orçun Aşık <asikorcun@gmail.com>
2025-03-04 19:38:38 +0300

    Created AboutController and Index page

---
+++ CoreOnlineEdu.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreOnlineEdu.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class AboutController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
