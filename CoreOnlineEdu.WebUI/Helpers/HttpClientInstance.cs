734df2b689cfc82605083493989be070e43ec1c7
Orçun Aşık <asikorcun@gmail.com>
2025-03-04 22:19:15 +0300

    static httpclient class helper added

---
+++ CoreOnlineEdu.WebUI/Helpers/HttpClientInstance.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreOnlineEdu.WebUI.Helpers;
+public static class HttpClientInstance
+{
+    public static HttpClient CreateClient()
+    {
+        HttpClient client = new();
+        client.BaseAddress = new Uri("https://localhost:7283/api/");
+        return client;
+    }
+}
