338ff725043429577398ffb5dd6213e66f48eeb2
Orçun Aşık <asikorcun@gmail.com>
2025-03-07 21:56:28 +0300

    BlogCategoryValidator classes for Create and Update

---
+++ CoreOnlineEdu.WebUI/Validators/CreateBlogCategoryValidator.cs
@@ -0,0 +1,14 @@
+﻿using CoreOnlineEdu.WebUI.Dtos.BlogCategoryDtos;
+using FluentValidation;
+
+namespace CoreOnlineEdu.WebUI.Validators;
+public class CreateBlogCategoryValidator : AbstractValidator<CreateBlogCategoryDto>
+{
+    public CreateBlogCategoryValidator()
+    {
+        RuleFor(bc => bc.CategoryName)
+            .NotEmpty().WithMessage("Kategori Adı Boş Geçilemez!")
+            .MaximumLength(60).WithMessage("Kategori Adı 60 Karakterden Fazla Olamaz!")
+            .MinimumLength(2).WithMessage("Kategori Adı 2 Karakterden Az Olamaz!");
+    }
+}
