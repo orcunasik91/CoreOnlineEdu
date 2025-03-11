307bbe67f131c13705381b834dac2abd3c102a58
Orçun Aşık <asikorcun@gmail.com>
2025-03-11 23:53:23 +0300

    AddJsonOptions set

--- CoreOnlineEdu.API/Program.cs
+++ CoreOnlineEdu.API/Program.cs
@@ -5,18 +5,24 @@ using CoreOnlineEdu.DataAccess.Context;
 using CoreOnlineEdu.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                });
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<OnlineEduContext>
     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-
+builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+builder.Services.AddScoped<IBlogService, BlogService>();
 
 var app = builder.Build();
 
