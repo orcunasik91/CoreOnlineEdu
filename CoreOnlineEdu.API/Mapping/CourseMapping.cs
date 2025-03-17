06176e99c0107b30a7983ff4e82952777756733e
Orçun Aşık <asikorcun@gmail.com>
2025-03-17 23:58:28 +0300

    edited course mapping types

--- CoreOnlineEdu.API/Mapping/CourseMapping.cs
+++ CoreOnlineEdu.API/Mapping/CourseMapping.cs
@@ -9,5 +9,9 @@ public class CourseMapping : Profile
     {
         CreateMap<CreateCourseDto, Course>().ReverseMap();
         CreateMap<UpdateCourseDto, Course>().ReverseMap();
+        CreateMap<Course, ResultCourseDto>().ReverseMap();
+        CreateMap<Course, ResultCourseWithCategoryDto>()
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.CourseCategory.CategoryName))
+            .ReverseMap();
     }
 }
