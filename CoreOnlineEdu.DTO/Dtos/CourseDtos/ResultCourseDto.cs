07a05d1b1e753feb0267fdf08eae95a898758702
Orçun Aşık <asikorcun@gmail.com>
2025-03-17 23:57:00 +0300

    Added ResultCourseDto, ResultCourseCategoryDto

---
+++ CoreOnlineEdu.DTO/Dtos/CourseDtos/ResultCourseDto.cs
@@ -0,0 +1,13 @@
+﻿using CoreOnlineEdu.DTO.Dtos.CourseCategoryDtos;
+
+namespace CoreOnlineEdu.DTO.Dtos.CourseDtos;
+public class ResultCourseDto
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public bool IsShown { get; set; }
+    public int CourseCategoryId { get; set; }
+    public ResultCourseCategoryDto CourseCategory { get; set; }
+}
---
+++ CoreOnlineEdu.DTO/Dtos/CourseDtos/ResultCourseWithCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreOnlineEdu.DTO.Dtos.CourseDtos;
+public class ResultCourseWithCategoryDto
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public string ImageUrl { get; set; }
+    public decimal Price { get; set; }
+    public bool IsShown { get; set; }
+    public string CategoryName { get; set; }
+}
