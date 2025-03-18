9081f1d233c8d6cc45da9ca1c2c43ec7b769a287
Orçun Aşık <asikorcun@gmail.com>
2025-03-18 11:59:56 +0300

    Created MessageController and Index,MessageDetail pages

---
+++ CoreOnlineEdu.WebUI/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,30 @@
+﻿using CoreOnlineEdu.WebUI.Dtos.MessageDtos;
+using CoreOnlineEdu.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreOnlineEdu.WebUI.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class MessageController : Controller
+{
+    private readonly HttpClient client = HttpClientInstance.CreateClient();
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var result = await client.GetFromJsonAsync<List<ResultMessageDto>>("Messages");
+        return View(result);
+    }
+
+    public async Task<IActionResult> RemoveMessage(int id)
+    {
+        await client.DeleteAsync($"Messages/{id}");
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> MessageDetail(int id)
+    {
+        var result = await client.GetFromJsonAsync<GetMessageByIdDto>($"Messages/{id}");
+        return View(result);
+    }
+}
