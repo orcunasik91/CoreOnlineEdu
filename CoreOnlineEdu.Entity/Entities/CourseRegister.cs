c6045ff9c428cb0cd00438135200ff8a423ad8fe
Orçun Aşık <asikorcun@gmail.com>
2025-04-27 23:20:29 +0300

    Created CourseRegister model

---
+++ CoreOnlineEdu.Entity/Entities/CourseRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreOnlineEdu.Entity.Entities;
+public class CourseRegister
+{
+    public int CourseRegisterId { get; set; }
+    public int CourseId { get; set; }
+    public Course Course { get; set; }
+    public int AppUserId { get; set; }
+    public AppUser AppUser { get; set; }
+}
