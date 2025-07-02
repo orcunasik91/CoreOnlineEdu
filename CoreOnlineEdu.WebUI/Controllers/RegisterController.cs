5f77f7b6057d2867e42c4e8592c9e08cc3f88213
Orçun Aşık <asikorcun@gmail.com>
2025-07-02 23:00:23 +0300

    Created register controller

---
+++ CoreOnlineEdu.WebUI/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using CoreOnlineEdu.WebUI.Dtos.UserDtos;
+using CoreOnlineEdu.WebUI.Services.UserServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreOnlineEdu.WebUI.Controllers;
+public class RegisterController(IUserService userService) : Controller
+{
+    [HttpGet]
+    public IActionResult Signup()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Signup(UserRegisterDto userRegisterDto)
+    {
+        var result = await userService.CreateUserAsync(userRegisterDto);
+        if (!result.Succeeded || !ModelState.IsValid)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View();
+        }
+        return RedirectToAction("Index","Login");
+    }
+}
